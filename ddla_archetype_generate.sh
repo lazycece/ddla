9343d500cce3c7c323a6f495ca0c4f2217322b7c
lazycece <lazycece@gmail.com>
2026-09-02 17:01:05 +0800

    mdf

--- ddla_archetype_generate.sh
+++ ddla_archetype_generate.sh
@@ -1,5 +1,5 @@
 #
-#    Copyright 2023 lazycece<lazycece@gmail.com>
+#    Copyright 2026 lazycece<lazycece@gmail.com>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+mvn clean
+
+cd ddla-samples/tradecore
 
 mvn archetype:create-from-project \
     -DpackageName=com.lazycece.tradecore \
